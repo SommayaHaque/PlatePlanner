243befa37634ce5508a96ac567bf9a123a1791b6
Sommaya Haque <haques4@bxscience.edu>
2020-06-23 14:31:25 -0400

    Initial Commit

---
+++ PlatePlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatePlanner
+//
+//  Created by Sommaya Haque on 6/23/20.
+//  Copyright © 2020 Sommaya Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
